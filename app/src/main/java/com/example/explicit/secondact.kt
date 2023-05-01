af494e791cb263f42630818defdfe72ef7001a9f
strange98350 <chandandutta168@gmail.com>
2023-05-01 18:57:29 +0530

    Add files in git

---
+++ app/src/main/java/com/example/explicit/secondact.kt
@@ -0,0 +1,11 @@
+package com.example.explicit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class secondact : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_secondact)
+    }
+}
